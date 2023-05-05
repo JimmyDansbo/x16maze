83431a8b9d05193b590a34f7c40489046c811837
Jimmy Dansbo <jimmy@dansbo.dk>
2023-05-05 11:20:05 +0200

    Added comments to main C source file

--- x16maze.c
+++ x16maze.c
@@ -11,58 +11,66 @@ typedef char s8;
 #define SCREEN_WIDTH 40
 #define SCREEN_HEIGHT 30
 
+// 16 bit value on ZeroPage that is updated by vSync interrupt
 #define myTimer 0x0002
 
+// Global variables
 u8 cursorx, cursory, bgcolor, curlvl;
 u16 lvlindex, remflds, MoveCnt;
 
+/******************************************************************************
+ Print a 0-terminated string starting at specified coordinates 
+******************************************************************************/
 static void printstr(u8 x, u8 y, char *str) {
 	u16 cnt=0;
-	u8 ch;
 
-	while (str[cnt]!=0) {
-		ch = str[cnt++];
-		if (ch > 0x3F) ch=ch-0x40;
-		PrintChar(x++, y, ch);
-	}
+	while (str[cnt]!=0)
+		PrintChar(x++, y, str[cnt++]);
 }
+
+/******************************************************************************
+ Print a 0-terminated string starting at specified coordinates with specified
+ foreground color 
+******************************************************************************/
 static void printstrfg(u8 x, u8 y, char *str, u8 fgcol) {
 	u16 cnt=0;
-	u8 ch;
 
 	while (str[cnt]!=0) {
-		ch = str[cnt++];
 		Setfgcol(x, y, fgcol);
-		PrintChar(x++, y, ch);
+		PrintChar(x++, y, str[cnt++]);
 	}
 }
+
+/******************************************************************************
+ Print a 0-terminated string starting at specified coordinates with specified
+ foreground- and background-color 
+******************************************************************************/
 static void printstrcol(u8 x, u8 y, char *str, u8 fgc, u8 bgc) {
 	u16 cnt=0;
-	u8 ch;
 	char col;
 
 	col = (bgc<<4)+fgc;
 
 	while (str[cnt]!=0) {
-		ch = str[cnt++];
 		Setcol(x, y, col);
-		PrintChar(x++, y, ch);
+		PrintChar(x++, y, str[cnt++]);
 	}
 }
 
+/******************************************************************************
+ Reset the playing field and ensure that text is written with correct color
+******************************************************************************/
 static void resetPlayfield() {
 	u8 x, y;
 
-	for (y=1; y<SCREEN_HEIGHT-1; y++)
-		for (x=1; x<SCREEN_WIDTH-1; x++) {
+	for (y=1; y<SCREEN_HEIGHT-1; ++y)
+		for (x=1; x<SCREEN_WIDTH-1; ++x) {
 			PrintChar(x, y, 0x20);
 			Setbgcol(x, y, WHITE);
 		}
-	for (y=2; y<SCREEN_HEIGHT-2; y++)
-		for (x=2; x<SCREEN_WIDTH-2; x++) {
-			PrintChar(x, y, 0x20);
+	for (y=2; y<SCREEN_HEIGHT-2; ++y)
+		for (x=2; x<SCREEN_WIDTH-2; ++x)
 			Setcol(x, y, (WALLCOL<<4)+BLACK);
-		}
 
 	printstrfg(2,1,"flds:000", RED);
 	printstrfg((SCREEN_WIDTH/2)-(4),1,"x16maze", RED);
@@ -70,41 +78,52 @@ static void resetPlayfield() {
 	printstrfg((SCREEN_WIDTH/2)-15,SCREEN_HEIGHT-2,"dpad=move b=next select=reset", RED);
 }
 
+/******************************************************************************
+ Set the border color by changing background color of top and bottom line as
+ well as rightmost and leftmost characters on screen.
+******************************************************************************/
 static void SetBorderColor(char color) {
 	u8 x, y;
 
-	for (x=0; x<SCREEN_WIDTH; x++) {
+	for (x=0; x<SCREEN_WIDTH; ++x) {
 		Setbgcol(x, 0, color);
 		Setbgcol(x, SCREEN_HEIGHT-1, color);
 	}
-	for (y=0; y<SCREEN_HEIGHT;y++) {
+	for (y=0; y<SCREEN_HEIGHT;++y) {
 		Setbgcol(0, y, color);
 		Setbgcol(SCREEN_WIDTH-1, y, color);
 	}
 }
 
+/******************************************************************************
+ Choose next background color, dependant on previous color
+ This function is used to "randomize" the border color
+******************************************************************************/
 static void nextbgcolor() {
 	switch (bgcolor) {
-		case WHITE:	bgcolor=RED;	break;
-		case RED:	bgcolor=CYAN;	break;
-		case CYAN:	bgcolor=PURPLE;	break;
-		case PURPLE:	bgcolor=GREEN;	break;
-		case GREEN:	bgcolor=BLUE;	break;
-		case BLUE:	bgcolor=YELLOW;	break;
-		case YELLOW:	bgcolor=ORANGE; break;
-		case ORANGE:	bgcolor=PINK;	break;
+		case WHITE:	bgcolor=RED;	    break;
+		case RED:	bgcolor=CYAN;	    break;
+		case CYAN:	bgcolor=PURPLE;	    break;
+		case PURPLE:	bgcolor=GREEN;	    break;
+		case GREEN:	bgcolor=BLUE;	    break;
+		case YELLOW:	bgcolor=ORANGE;     break;
+		case BLUE:	bgcolor=YELLOW;	    break;
+		case ORANGE:	bgcolor=PINK;	    break;
 		case PINK:	bgcolor=LIGHTGREEN; break;
-		case LIGHTGREEN:bgcolor=LIGHTBLUE; break;
-		case LIGHTBLUE:	bgcolor=WHITE;	break;
+		case LIGHTGREEN:bgcolor=LIGHTBLUE;  break;
+		case LIGHTBLUE:	bgcolor=WHITE;	    break;
 	}
 }
 
+/******************************************************************************
+ Show the splash screen
+******************************************************************************/
 static void splashscreen() {
 	u8 x, y;
 	u8 btn=0;
 
-	for (y=0;y<SCREEN_HEIGHT;y++)
-		for (x=0;x<SCREEN_WIDTH;x++) {
+	for (y=0; y<SCREEN_HEIGHT; ++y)
+		for (x=0; x<SCREEN_WIDTH; ++x) {
 			PrintChar(x, y, ' ');
 			Setbgcol(x, y, BLACK);
 		}
@@ -126,6 +145,7 @@ static void splashscreen() {
 	printstrfg((SCREEN_WIDTH/2)-11, (SCREEN_HEIGHT/2)+5, "(jimmy@dansbo.dk)  2023", GREEN);
 	printstrfg((SCREEN_WIDTH/2)-15, (SCREEN_HEIGHT/2)+7, "https://github.com/jimmydansbo/", LIGHTGREEN);
 
+	// Wait for user to press start. Use the time to "randomize" background color
 	while (btn != SNES_STA) {
 		waitVsync();
 		nextbgcolor();
@@ -133,19 +153,25 @@ static void splashscreen() {
 	}
 }
 
+/******************************************************************************
+ Find index of start of level specified in curlvl variable
+******************************************************************************/
 static void seeklevel() {
 	u8 lvl=1;
 
 	lvlindex = 0;
 
+	// Loop while lvl != curlvl
 	while (lvl++ != curlvl) {
+		// If first value of level is 0, we have reached the end of levels
 		if (levels[lvlindex]==0) {
-			curlvl=1;
-			lvlindex=0;
+			curlvl=1;	// Set current level to 1 and
+			lvlindex=0;	// lvlindex to 0
 			return;
 		}
-		lvlindex += levels[lvlindex];
+		lvlindex += levels[lvlindex];	// Set next lvlindex
 	}
+	// If first value of level is 0, we have reached the end of levels
 	if (levels[lvlindex]==0) {
 		curlvl=1;
 		lvlindex=0;
@@ -153,6 +179,9 @@ static void seeklevel() {
 	}
 }
 
+/******************************************************************************
+ Draw the current level to screen
+******************************************************************************/
 static void drawlevel() {
 	u8 ch, bitcnt;
 	u8 offsetx, offsety, datacnt;
@@ -163,11 +192,14 @@ static void drawlevel() {
 
 	SetBorderColor(bgcolor);
 
+	// Calculate offset of top left corner from the size of the maze
 	offsetx = (SCREEN_WIDTH/2)-(levels[lvlindex+1]/2);
 	offsety = (SCREEN_HEIGHT/2)-(levels[lvlindex+2]/2);
 
 	datacnt=0;
 	bitcnt=0;
+
+	// Draw the maze it self
 	ch=levels[lvlindex+5+datacnt++];
 	for (cury=offsety; cury<offsety+levels[lvlindex+2]; cury++) {
 		if (bitcnt!=0) {
@@ -187,49 +219,73 @@ static void drawlevel() {
 		}
 	}
 
+	// Place the cursor at the starting position
 	cursorx = offsetx+levels[lvlindex+3];
 	cursory = offsety+levels[lvlindex+4];
 
 	PrintChar(cursorx, cursory, 0x00); // Non-printable character
 	Setbgcol(cursorx, cursory, bgcolor);
-	remflds--;
+
+	// remflds has been incremented once too manu
+	--remflds;
 	sprintf(str, "%03d", remflds);
 	printstr(7, 1, str);
+	// Print current level
 	sprintf(str, "%03d", curlvl);
 	printstr(SCREEN_WIDTH-5, 1, str);
+	// Reset timer variable and MoveCnt
 	*(unsigned *)myTimer=0;
 	MoveCnt=0;
 }
 
+/******************************************************************************
+ Move cursor according to the X and Y parameters
+ X and Y parametes should be either -1, 0 or +1
+ -1 decrements a coordinate (making cursor go up or left)
+ 0 leaves coordinate alone
+ +1 increments a coordinate (making cursor go down or right)
+******************************************************************************/
 static void do_move(s8 x, s8 y) {
 	u8 moved=false;
 	char str[5];
 
+	// Loop until we hit a wall
 	while (Getbgcol(cursorx+x, cursory+y)!=WALLCOL) {
+		// Remove old cursor 
 		PrintChar(cursorx, cursory, ' ');
+		// Calculate new coordinate
 		cursorx+=x;
 		cursory+=y;
 		moved=true;
+		// Only update remaining fields if we overwrite
+		// an empty field
 		if (Getbgcol(cursorx, cursory)==BLACK) {
 			remflds--;
 			sprintf(str, "%03d", remflds);
 			printstr(7, 1, str);
 		}
+		// Show cursor at new location
 		PrintChar(cursorx, cursory, 0x00);  //non-printable
 		Setbgcol(cursorx, cursory, bgcolor);
 		waitVsync();
 	}
+	// Update MoveCnt if we have actually moved
 	if (moved) MoveCnt++;
 }
 
+/******************************************************************************
+ Show statistics for the completed level
+******************************************************************************/
 static void show_win() {
 	u8 btn=0;
 	u8 hour, minute, second;
 	u16 msec, curtimer;
 	char str[40];
 
+	// Read the current jiffie time
 	curtimer=*(unsigned *)myTimer;
 
+	// Calculate human readable values
 	hour = curtimer/60/60/60;
 	curtimer=curtimer-(hour*60*60*60);
 	minute = curtimer/60/60;
@@ -238,8 +294,10 @@ static void show_win() {
 	curtimer=curtimer-(second*60);
 	msec=curtimer*(1000/60);
 
+	// Print the values in a formatted string
 	sprintf(str, "* %02d hr %02d min %02d sec %03d msec *",hour,minute,second,msec);
 
+	// Show the statistics
 	printstrcol((SCREEN_WIDTH/2)-16, (SCREEN_HEIGHT/2)-4,  "********************************", ORANGE, BLACK);
 	printstrcol((SCREEN_WIDTH/2)-16, (SCREEN_HEIGHT/2)-3,  "*                              *", ORANGE, BLACK);
 	printstrcol((SCREEN_WIDTH/2)-16, (SCREEN_HEIGHT/2)-2,  "*       level completed        *", ORANGE, BLACK);
@@ -251,8 +309,11 @@ static void show_win() {
 	sprintf(str,"*          %04d moves          *",MoveCnt);
 	printstrcol((SCREEN_WIDTH/2)-16, (SCREEN_HEIGHT/2)+2, str, ORANGE,BLACK);
 
-	curtimer=*(unsigned *)myTimer;	
+	// Use the timer as a delay when updating the "bordercolor"
+	curtimer=*(unsigned *)myTimer;
+	// Wait for user to press the B button
 	while (btn != SNES_B) {
+		// Cycle bgcolor's
 		nextbgcolor();
 		SetBorderColor(bgcolor);
 		while ((*(unsigned *)myTimer != curtimer+10) && (btn != SNES_B)) {
@@ -263,12 +324,16 @@ static void show_win() {
 	}
 }
 
+/******************************************************************************
+ Show window to let the user select a specific level
+******************************************************************************/
 static void select_level() {
 	u8 btnPressed=0;
 	u8 btnHeld=0;
 	u8 btnPrev=0;
 	char str[6];
 
+	// Show the window
 	printstrcol(12, 9, "*****************", ORANGE, BLACK);
 	printstrcol(12,10, "*               *", ORANGE, BLACK);
 	printstrcol(12,11, "* select level: *", ORANGE, BLACK);
@@ -276,19 +341,25 @@ static void select_level() {
 	printstrcol(12,13, "*               *", ORANGE, BLACK);
 	printstrcol(12,14, "*               *", ORANGE, BLACK);
 	printstrcol(12,15, "*****************", ORANGE, BLACK);
+	// Update with the current level
 	sprintf(str, "%03d", curlvl);
 	printstr(19, 13, str);
 
+	// Let the user use up- and down arrows to select level
+	// Loop until user presses SELECT
 	while (btnPressed != SNES_SEL) {
 		waitVsync();
+		// Use 3 variables to decide if a button is held
 		btnHeld = ReadJoypad(0);
 		btnPressed = ReadJoypad(0);
 		btnPressed = btnHeld & (btnHeld ^ btnPrev);
 
+		// If DOWN has been pressed, decrement level
 		if (btnPressed & SNES_DN) {
 			if (--curlvl == 0) curlvl=MAX_LEVELS;
 			sprintf(str, "%03d", curlvl);
 			printstr(19, 13, str);
+		// If UP has been pressed, increment level
 		} else if (btnPressed & SNES_UP) {
 			if (++curlvl > MAX_LEVELS) curlvl=1;
 			sprintf(str, "%03d", curlvl);
@@ -298,6 +369,9 @@ static void select_level() {
 	}
 }
 
+/******************************************************************************
+ Main function that initializes game and runs the main loop
+******************************************************************************/
 int main(){
 	u8 btnPressed;
 	u8 btnHeld=0;
@@ -314,11 +388,14 @@ int main(){
 	*(unsigned *)myTimer = 0;
 	start_timer();
 
+	// Show the splash screen and wait for the user to press START
 	splashscreen();
 
 	while (1) {
+		// Find curlvl
 		seeklevel();
 		resetPlayfield();
+		// Draw curlvl
 		drawlevel();
 		btnPressed=0;
 		while (btnPressed != SNES_SEL) {
@@ -340,6 +417,7 @@ int main(){
 				btnPressed=SNES_SEL;
 			}
 			btnPrev = btnHeld;
+			// If there are no more fields, the maze is solved
 			if (remflds==0) {
 				show_win();
 				curlvl++;

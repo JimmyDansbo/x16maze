888bc77e1f8f1440fc1a5ecd89da982d627c29f1
Jimmy Dansbo <jimmy@dansbo.dk>
2023-05-13 08:14:47 +0200

    Removed MAX_LEVEL definition as seeklevel function can provide this seeklevel function will be rewritten to return the highest level number if a level that is too high has been provided

--- levels.h
+++ levels.h
@@ -107,8 +107,6 @@ below to see how they are done or you can contact the author of this program
 on the Uzebox forum or mail: jimmy at dansbo.dk
 */
 
-#define MAX_LEVELS 58
-
 const unsigned char levels[] = {
 // Level 1
 25,10,10,		// Size,Width,Height
